520c2411b842d44ee7c6b441016ab5c104696c68
H0n0riuss <joscha.reichenberg@web.de>
2024-06-23 13:05:06 +0200

    :recycle: remove Type import in TrackingAspect

--- src/main/java/io/github/honoriuss/blossom/TrackingAspect.java
+++ src/main/java/io/github/honoriuss/blossom/TrackingAspect.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 
 @Aspect
 @Component
@@ -49,8 +48,8 @@ class TrackingAspect<T> { //TODO null checks
 
     private void compareGenericParams(ITrackingHandler<T> trackingHandler,
                                       ITrackingObjectMapper<T> trackingObjectMapper) {
-        Type handlerType = ((ParameterizedType) trackingHandler.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
-        Type mapperType = ((ParameterizedType) trackingObjectMapper.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
+        var handlerType = ((ParameterizedType) trackingHandler.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
+        var mapperType = ((ParameterizedType) trackingObjectMapper.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
         logger.info("Generic types from handler ({}) and  mapper({}) are compatible: {}", handlerType, mapperType, handlerType.equals(mapperType));
     }
 }
