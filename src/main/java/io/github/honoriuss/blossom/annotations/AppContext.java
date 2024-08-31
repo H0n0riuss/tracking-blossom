77ba9f457f3dff179dfc2969ae484907a0fdad87
H0n0riuss <joscha.reichenberg@web.de>
2024-08-31 17:44:18 +0200

    :recycle: AppContext default app name lower

--- src/main/java/io/github/honoriuss/blossom/annotations/AppContext.java
+++ src/main/java/io/github/honoriuss/blossom/annotations/AppContext.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface AppContext {
     String appKey() default "app";
-    String app() default "Unknown";
+    String app() default "unknown";
 }
