682a66d9941cbe77f90a0ea92eb1824646e8ed69
H0n0riuss <joscha.reichenberg@web.de>
2025-04-10 11:11:10 +0200

    :sparkles: adding componentscan

--- src/main/java/io/github/honoriuss/blossom/BlossomConfig.java
+++ src/main/java/io/github/honoriuss/blossom/BlossomConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan(basePackages = "io.github.honoriuss.blossom")
 class BlossomConfig<T> {
 
     @Bean
