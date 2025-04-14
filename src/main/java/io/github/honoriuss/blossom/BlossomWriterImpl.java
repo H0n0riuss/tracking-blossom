af6c7241f9c40ad6c3a55ad386f7f611f8ead0c4
H0n0riuss <joscha.reichenberg@web.de>
2025-04-14 15:25:27 +0200

    :sparkles: adding Mono and Flux Writer

--- src/main/java/io/github/honoriuss/blossom/BlossomWriterImpl.java
+++ src/main/java/io/github/honoriuss/blossom/BlossomWriterImpl.java
@@ -3,6 +3,8 @@ package io.github.honoriuss.blossom;
 import io.github.honoriuss.blossom.interfaces.ITrackingWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 class BlossomWriterImpl<T> implements ITrackingWriter<T> {
     private final Logger logger = LoggerFactory.getLogger(BlossomWriterImpl.class);
@@ -10,5 +12,15 @@ class BlossomWriterImpl<T> implements ITrackingWriter<T> {
     @Override
     public void write(T message) {
         logger.info("write: {}", message);
+    }
+
+    @Override
+    public Mono<T> writeMono(T message) {
+        return Mono.just(message);
+    }
+
+    @Override
+    public Flux<T> writeFlux(T message) {
+        return Flux.just(message);
     }
 }
--- src/main/java/io/github/honoriuss/blossom/interfaces/ITrackingWriter.java
+++ src/main/java/io/github/honoriuss/blossom/interfaces/ITrackingWriter.java
@@ -1,5 +1,17 @@
 package io.github.honoriuss.blossom.interfaces;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ITrackingWriter<T> {
-    void write(T message);
+    default void write(T message) {
+    }
+
+    default Mono<T> writeMono(T message) {
+        return Mono.empty();
+    }
+
+    default Flux<T> writeFlux(T message) {
+        return Flux.empty();
+    }
 }
